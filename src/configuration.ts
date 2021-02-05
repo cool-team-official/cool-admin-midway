b8f660d1cdd4ee6678d23961c7a0d66589de06ab
啊平 <951984189@qq.com>
2021-02-05 19:04:53 +0800

    11

--- src/configuration.ts
+++ src/configuration.ts
@@ -6,11 +6,14 @@ import * as cool from 'midwayjs-cool-core';
 import * as redis from 'midwayjs-cool-redis';
 
 @Configuration({
+  // 注意组件顺序 cool 有依赖orm组件， 所以必须放在，orm组件之后 cool的其他组件必须放在cool 核心组件之后
   imports: [
-    // cool-admin 官方组件 https://www.cool-js.com
+    // 必须，不可移除， https://typeorm.io  打不开？ https://typeorm.biunav.com/zh/
+    orm,
+    // 必须，不可移除， cool-admin 官方组件 https://www.cool-js.com
     cool,
-    redis,
-    orm
+    redis
+    
   ]
 })
 export class ContainerLifeCycle implements ILifeCycle {
