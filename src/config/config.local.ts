d55d3c95828a39caf75776f2d2275e3a03abb5c6
ap <951984189@qq.com>
2021-02-28 01:50:58 +0800

    升级核心依赖

--- src/config/config.local.ts
+++ src/config/config.local.ts
@@ -10,8 +10,8 @@ export default (appInfo: EggAppInfo) => {
         host: '127.0.0.1',
         port: 3306,
         username: 'root',
-        password: '',
-        database: 'cool-admin',
+        password: '123123',
+        database: 'cool',
         // 自动建表 注意：线上部署的时候不要使用，有可能导致数据丢失
         synchronize: true,
         // 打印日志
