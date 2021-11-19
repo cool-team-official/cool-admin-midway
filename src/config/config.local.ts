242193046b8cc07345c713d1ae3399fefc81494d
ap <951984189@qq.com>
2021-11-19 11:45:34 +0800

    升级核心依赖

--- src/config/config.local.ts
+++ src/config/config.local.ts
@@ -7,11 +7,11 @@ export default (appInfo: EggAppInfo) => {
 
   config.orm = {
     type: 'mysql',
-    host: '10.211.55.7',
+    host: '127.0.0.1',
     port: 3306,
-    username: 'midway',
+    username: 'root',
     password: '123123',
-    database: 'midway',
+    database: 'cool',
     // 自动建表 注意：线上部署的时候不要使用，有可能导致数据丢失
     synchronize: true,
     // 打印日志
