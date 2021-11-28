2dd8a1954b532a0ea682e9c3eb3bc283cdc3998b
啊平 <951984189@qq.com>
2021-11-28 21:39:42 +0800

    4.x 就绪

--- src/config/config.local.ts
+++ src/config/config.local.ts
@@ -10,7 +10,7 @@ export default (appInfo: EggAppInfo) => {
     host: '127.0.0.1',
     port: 3306,
     username: 'root',
-    password: '123123',
+    password: '123456',
     database: 'cool',
     // 自动建表 注意：线上部署的时候不要使用，有可能导致数据丢失
     synchronize: true,
@@ -18,6 +18,8 @@ export default (appInfo: EggAppInfo) => {
     logging: true,
     // 字符集
     charset: 'utf8mb4',
+    // 驱动
+    driver: require("mysql2")
   };
 
   config.logger = {
