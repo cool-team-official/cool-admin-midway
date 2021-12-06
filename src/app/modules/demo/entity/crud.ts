2be4730437160c35883ddedccc97820e2ecb91ee
ap <951984189@qq.com>
2021-12-06 18:41:44 +0800

    4.x 就绪

--- src/app/modules/demo/entity/crud.ts
+++ src/app/modules/demo/entity/crud.ts
@@ -17,6 +17,25 @@ export class DemoCrudEntity extends BaseEntity {
   @Column({ comment: '年龄' })
   age: number;
 
-  @Column({ type: 'tinyint', comment: '性别 0-未知 1-男 2-女', default: 0 })
+  @Column({ comment: '出生日期' })
+  birthDate: Date;
+
+  @Column({ comment: '出生日期1' })
+  birthTime: Date;
+
+  @Column({ comment: '出生日期2' })
+  startTime: Date;
+
+  @Column({ comment: '出生日期e' })
+  endTime: Date;
+
+  @Column({
+    type: 'tinyint',
+    comment: '性别 0-未知 1-男 2-女',
+    default: 0,
+  })
   type: number;
+
+  @Column({ comment: '备注', nullable: true })
+  remark: string;
 }
