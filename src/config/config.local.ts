33ec109dda6c5953998c56e1949af70ee2da2064
ap <951984189@qq.com>
2022-01-06 17:40:15 +0800

    完善定时任务

--- src/config/config.local.ts
+++ src/config/config.local.ts
@@ -15,7 +15,7 @@ export default (appInfo: EggAppInfo) => {
     // 自动建表 注意：线上部署的时候不要使用，有可能导致数据丢失
     synchronize: true,
     // 打印日志
-    logging: false,
+    logging: true,
     // 字符集
     charset: 'utf8mb4',
     // 驱动
--- src/configuration.ts
+++ src/configuration.ts
@@ -5,9 +5,9 @@ import * as view from '@midwayjs/view-nunjucks';
 import * as orm from '@midwayjs/orm';
 import * as cool from '@cool-midway/core';
 // import * as wxpay from '@cool-midway/wxpay';
-import * as oss from '@cool-midway/oss';
-import * as redis from '@cool-midway/redis';
-import * as queue from '@cool-midway/queue';
+// import * as oss from '@cool-midway/oss';
+// import * as redis from '@cool-midway/redis';
+// import * as queue from '@cool-midway/queue';
 // import * as alipay from '@cool-midway/alipay';
 // import * as socket from '@cool-midway/socket';
 
@@ -21,11 +21,11 @@ import * as queue from '@cool-midway/queue';
     // 必须，不可移除， cool-admin 官方组件 https://www.cool-js.com
     cool,
     // oss插件，需要到后台配置之后才有用，默认是本地上传
-    oss,
+    // oss,
     // 将缓存替换成redis
-    redis,
-    // 队列
-    queue,
+    // redis,
+    // // 队列
+    // queue,
     // 微信支付
     //wxpay,
     // 支付宝支付
