e0e86cdf6b882e33099cf628074febae62b278ee
ap <951984189@qq.com>
2022-01-10 18:28:42 +0800

    集成swagger

--- src/app/modules/demo/controller/app/swagger.ts
+++ src/app/modules/demo/controller/app/swagger.ts
@@ -14,10 +14,16 @@ export class DemoSwaggerController extends BaseController {
   @CreateApiDoc()
     .summary('hello 接口')
     .description('hello 接口功能描述')
-    .param('姓名')
+    .param('姓名', { required: true })
+    .param('年龄', { required: true })
+    .param('简介', { required: false })
     .build()
   @Get('/hello')
-  async hello(@Query() name: string) {
-    return this.ok(`你好：${name}!!`);
+  async hello(
+    @Query() name: string,
+    @Query() age: number,
+    @Query() desc: string
+  ) {
+    return this.ok(`你好：${name}!! ${age} ${desc}`);
   }
 }
