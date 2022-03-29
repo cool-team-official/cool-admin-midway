206151a01640834325f0b29d6a150895ee1dc5d7
ap <951984189@qq.com>
2022-03-29 09:34:09 +0800

    新增urlTag

--- src/modules/demo/controller/app/tag.ts
+++ src/modules/demo/controller/app/tag.ts
@@ -1,9 +1,10 @@
-import { Provide } from '@midwayjs/decorator';
+import { Get, Inject, Provide } from '@midwayjs/decorator';
 import {
   CoolController,
   BaseController,
   CoolUrlTag,
   TagTypes,
+  CoolUrlTagData,
 } from '@cool-midway/core';
 
 /**
@@ -16,7 +17,19 @@ import {
   pageQueryOp: () => {},
 })
 @CoolUrlTag({
-  key: TagTypes.ignoreToken,
-  value: [],
+  key: TagTypes.IGNORE_TOKEN,
+  value: ['add'],
 })
-export class DemoAppTagController extends BaseController {}
+export class DemoAppTagController extends BaseController {
+  @Inject()
+  tag: CoolUrlTagData;
+
+  /**
+   * 获得标签数据， 如可以标记忽略token的url，然后在中间件判断
+   * @returns
+   */
+  @Get('/data')
+  async data() {
+    return this.ok(this.tag.byKey(TagTypes.IGNORE_TOKEN));
+  }
+}
