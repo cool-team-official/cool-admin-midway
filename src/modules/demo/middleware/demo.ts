5023b904e2f055ba4a529c5e1d13e0d291d8ab7a
ap <951984189@qq.com>
2022-03-29 16:12:00 +0800

    urltag

--- src/modules/demo/middleware/demo.ts
+++ src/modules/demo/middleware/demo.ts
@@ -1,11 +1,19 @@
+import { CoolUrlTagData, TagTypes } from '@cool-midway/core';
 import { IMiddleware } from '@midwayjs/core';
-import { Middleware } from '@midwayjs/decorator';
+import { Inject, Middleware } from '@midwayjs/decorator';
 import { NextFunction, Context } from '@midwayjs/koa';
 
 @Middleware()
 export class DemoMiddleware implements IMiddleware<Context, NextFunction> {
+  @Inject()
+  tag: CoolUrlTagData;
+
   resolve() {
     return async (ctx: Context, next: NextFunction) => {
+      const urls = this.tag.byKey(TagTypes.IGNORE_TOKEN);
+
+      console.log('忽略token的URL数组', urls);
+
       // 控制器前执行的逻辑
       const startTime = Date.now();
       // 执行下一个 Web 中间件，最后执行到控制器
