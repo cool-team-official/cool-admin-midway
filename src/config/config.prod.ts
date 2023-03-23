900d7a69d7e8f2aee6152cdc3b4bcc42ada9a13f
啊平 <951984189@qq.com>
2023-03-23 18:31:17 +0800

    修改提示语

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -2,7 +2,7 @@ import { CoolConfig } from '@cool-midway/core';
 import { MidwayConfig } from '@midwayjs/core';
 
 /**
- * 本地开发 npm run dev 读取的配置文件
+ * 本地开发 npm run prod 读取的配置文件
  */
 export default {
   typeorm: {
