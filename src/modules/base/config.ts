9f0034bd84db23af7a758098f0da09acdce1de17
无风 <team@cool-js.com>
2023-04-21 18:27:07 +0800

    完善

--- src/modules/base/config.ts
+++ src/modules/base/config.ts
@@ -20,7 +20,7 @@ export default () => {
       // 单点登录
       sso: false,
       // 注意： 最好重新修改，防止破解
-      secret: 'FOAPOFALOEQIPNNLQ',
+      secret: 'FJPOJSFPAFKPA',
       // token
       token: {
         // 2小时过期，需要用刷新token
