26b0e37dc2a4235dd9ce53c3223e0df347ab3431
cool_team <cool_team@163.com>
2023-06-02 18:00:53 +0800

    更新依赖到最新

--- src/modules/task/queue/task.ts
+++ src/modules/task/queue/task.ts
@@ -1,7 +1,6 @@
 import { App, Inject } from '@midwayjs/decorator';
 import { BaseCoolQueue, CoolQueue } from '@cool-midway/task';
 import { TaskInfoService } from '../service/info';
-import { Job } from 'bullmq';
 import { IMidwayApplication } from '@midwayjs/core';
 
 /**
@@ -15,7 +14,7 @@ export abstract class TaskInfoQueue extends BaseCoolQueue {
   @Inject()
   taskInfoService: TaskInfoService;
 
-  async data(job: Job, done: any): Promise<void> {
+  async data(job, done: any): Promise<void> {
     try {
       const result = await this.taskInfoService.invokeService(job.data.service);
       this.taskInfoService.record(job.data, 1, JSON.stringify(result));
