ad1bfdad6397bdc4cf57b13316606bec3df0d096
cool <cool_team@163.com>
2023-11-22 10:00:28 +0800

    新增swagger支持

--- src/modules/base/config.ts
+++ src/modules/base/config.ts
@@ -22,7 +22,7 @@ export default () => {
       // 单点登录
       sso: false,
       // 注意： 最好重新修改，防止破解
-      secret: '18d9ed80852a11eebe1a49eaab5535f2',
+      secret: 'cool-admin-xxxxxx',
       // token
       token: {
         // 2小时过期，需要用刷新token
