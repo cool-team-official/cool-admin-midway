ec2f6e3bfe08d79940127a81c746c3a9d68b6f38
17691002584 <17691002584@163.com>
2026-07-11 23:06:44 +0800

    chore(config): update prod db username and password
    also add generated bundle entry index.ts

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -98,8 +98,8 @@ export default {
         type: 'mysql',
         host: '127.0.0.1',
         port: 3306,
-        username: 'cms',
-        password: 'n4mM4KHkEyaNZEm2',
+        username: 'root',
+        password: 'admin',
         database: 'cms',
         // 自动建表 注意：线上部署的时候不要使用，有可能导致数据丢失
         synchronize: false,
